4b1ad6ae6774b610c470d97e03cc96e5abc7cd9f
Mieko Yao <mieko.yao@mail.utoronto.ca>
2022-12-03 14:22:37 -0500

    fixed error placement on signup page

--- PF/fitness-club/src/pages/signup/index.js
+++ PF/fitness-club/src/pages/signup/index.js
@@ -16,72 +16,37 @@ const SignUp = () => {
     const [avatar, setAvatar] = useState(null);
     const [errors, setErrors] = useState({});
 
-    function validate (e){
+    function validate_all(){
         let currErrors = {};
-        switch (e.target.id){
-            case 'first_name':
-                if (e.target.value.length > 2) {
-                    delete currErrors[first];
-                    setFirst(e.target.value);
-                }
-                else {
-                    currErrors[first] = "Please enter a first name."
-                }
-                break;
-            case 'last_name':
-                if (e.target.value.length > 2) {
-                    delete currErrors[last];
-                    setLast(e.target.value);
-                }
-                else {
-                    currErrors[last] = "Please enter a last name."
-                }
-                break;
-            case 'email':
-                let valid_email = /^([A-Za-z0-9!#$%&'*+\-\/=?^_`{|}~]|\.[A-Za-z0-9!#$%&'*+\-\/=?^_`{|}~]){1,64}@(([A-Za-z0-9][A-Za-z0-9\-]{0,61}[A-Za-z0-9]{0,1}\.){1,}[A-Za-z]{1,10}|[A-Za-z0-9][A-Za-z0-9\-]{0,61}[A-Za-z0-9]|[A-Za-z0-9]{1,61})$/.test(e.target.value);
-                if (valid_email) {
-                    delete currErrors[email];
-                    setEmail(e.target.value);
-                }
-                else {
-                    currErrors[email] = "Please enter a valid email."
-                }
-                break;
-            case 'phone':
-                let valid_phone = /^\d{10}$/.test(e.target.value);
-                if (valid_phone){
-                    delete currErrors[phone];
-                    setPhone(e.target.value);
-                }
-                else {
-                    currErrors[phone] = "Please enter a valid phone (numbers only)."
-                }
-                break;
-            case 'password':
-                if (e.target.value.length > 3){
-                    delete currErrors[password1];
-                    setPassword1(e.target.value);
-                }
-                else {
-                    currErrors[password1] = "Please enter a password."
-                }
-                break;
-            case 'password2':
-                console.log(password1);
-                console.log(e.target.value);
-                let match_pass = password1 === e.target.value;
-                if (match_pass){
-                    delete currErrors[password2];
-                    setPassword2(e.target.value);
-                } else {
-                    currErrors[password2] = "Passwords don't match."
-                }
-                break;
-            default:
-                break;
+        if (first === ''){
+            currErrors["first"] = "Please enter a first name."
         }
+        if (last === ''){
+            currErrors["last"] = "Please enter a last name."
+        }
+
+        let valid_email = /^((\w|[!#$%&'*+\-\/=?^`{|}~])(\.\w|\.[!#$%&'*+\-\/=?^`{|}~])?){1,64}@([A-Za-z0-9](-?[A-Za-z0-9]|\.?[A-Za-z0-9])*)+$/
+        if (email === ''|| !valid_email.test(email)){
+            currErrors["email"] = "Please enter a valid email."
+        }
+
+        let valid_phone = /^\d{10}$/
+        if (phone === '' || !valid_phone.test(phone)){
+            currErrors["phone"] = "Please enter a valid phone number."
+        }
+
+        if (password1 === ''){
+            currErrors["password1"] = "Please enter a password."
+        }
+
+        if (password2 === '' || password1 !== password2){
+            currErrors["password2"] = "Passwords don't match."
+        }
+
         setErrors(currErrors);
+        return (Object.entries(currErrors).length === 0);
     }
+
 
     function uploadAvatar (e) {
         // const img = new File([e.target.files[0]], e.target.files[0].name, {
@@ -92,60 +57,57 @@ const SignUp = () => {
     }
 
     const submitReq = () => {
-        let form_data = new FormData();
-        form_data.append('email', email);
-        form_data.append('password', password1);
-        form_data.append('password2', password2);
-        form_data.append('first_name', first);
-        form_data.append('last_name', last);
-        form_data.append('phone', phone);
-        form_data.append('avatar', avatar);
+        if (validate_all()) {
 
-        fetch("http://localhost:8000/accounts/signup/", {
-            method: 'POST',
-            // headers: {
-            //     'Content-Type': 'multipart/form-data',
-            // },
-            // body: JSON.stringify({
-            //     "email": email,
-            //     "password": password1,
-            //     "password2": password2,
-            //     "first_name": first,
-            //     "last_name": last,
-            //     "phone": phone,
-            //     "avatar": avatar,
-            // })
-            body: form_data,
-        }).then(response => response.json())
-            .then(response => {
+            let form_data = new FormData();
+            form_data.append('email', email);
+            form_data.append('password', password1);
+            form_data.append('password2', password2);
+            form_data.append('first_name', first);
+            form_data.append('last_name', last);
+            form_data.append('phone', phone);
+            form_data.append('avatar', avatar);
 
-                console.log(response)
+            fetch("http://localhost:8000/accounts/signup/", {
+                method: 'POST',
+                // headers: {
+                //     'Content-Type': 'multipart/form-data',
+                // },
+                // body: JSON.stringify({
+                //     "email": email,
+                //     "password": password1,
+                //     "password2": password2,
+                //     "first_name": first,
+                //     "last_name": last,
+                //     "phone": phone,
+                //     "avatar": avatar,
+                // })
+                body: form_data,
+            }).then(response => response.json())
+                .then(response => {
 
-            }).catch(error => console.log(error));
+                    console.log(response)
+
+                }).catch(error => console.log(error));
+        }
     }
-
-    const required_fields = (first !== "") && (last !== "") && (phone !== "") && (email !== "") && (password1 !== "")
-        && (password2 !== "") && (Object.entries(errors).length === 0);
 
     return (
         <>
             <form>
                 <h1>Sign Up</h1>
-                <ul>
-                    {Object.entries(errors).map(([prop, value]) => {
-                        return (
-                            <li className='error-message' key={prop}>
-                                {value}
-                            </li>
-                        );
-                    })}
-                </ul>
-                <TextField id="first_name" label="First Name" variant="outlined" required onChange={validate}/>
-                <TextField id="last_name" label="Last Name" variant="outlined" required onChange={validate}/>
-                <TextField id="email" label="Email" variant="outlined" type="email" required onChange={validate}/>
-                <TextField id="phone" label="Phone" variant="outlined" required onChange={validate}/>
-                <TextField id="password" label="Password" type="password" variant="outlined" required onChange={validate}/>
-                <TextField id="password2" label="Confirm Password" variant="outlined" type="password" required  onChange={validate}/>
+                <TextField id="first_name" label="First Name" variant="outlined" required onChange={e => setFirst(e.target.value)}
+                           error={errors.first !== undefined} helperText={errors.first}/>
+                <TextField id="last_name" label="Last Name" variant="outlined" required onChange={e => setLast(e.target.value)}
+                           error={errors.last !== undefined} helperText={errors.last}/>
+                <TextField id="email" label="Email" variant="outlined" type="email" required onChange={e => setEmail(e.target.value)}
+                           error={errors.email !== undefined} helperText={errors.email}/>
+                <TextField id="phone" label="Phone (Numbers only)" variant="outlined" required onChange={e => setPhone(e.target.value)}
+                           error={errors.phone !== undefined} helperText={errors.phone}/>
+                <TextField id="password" label="Password" type="password" variant="outlined" required onChange={e => setPassword1(e.target.value)}
+                           error={errors.password1 !== undefined} helperText={errors.password2}/>
+                <TextField id="password2" label="Confirm Password" variant="outlined" type="password" required  onChange={e => setPassword2(e.target.value)}
+                           error={errors.password2 !== undefined} helperText={errors.password2}/>
                 <input
                     accept="image/*"
                     style={{ display: 'none' }}
@@ -159,7 +121,7 @@ const SignUp = () => {
                     </Button>
                 </label>
 
-                <Button id="submit-button" variant="contained" onClick={submitReq} disabled={!required_fields}>Sign Up</Button>
+                <Button id="submit-button" variant="contained" onClick={submitReq}>Sign Up</Button>
             </form>
 
         </>
