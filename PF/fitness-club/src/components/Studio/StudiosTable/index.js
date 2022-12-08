e930c98c98106d205d056e851753da37af46147e
Hala Sheta <h.sheta@mail.utoronto.ca>
2022-12-08 00:02:32 -0500

    studio filtering & pagination

--- PF/fitness-club/src/components/Studio/StudiosTable/index.js
+++ PF/fitness-club/src/components/Studio/StudiosTable/index.js
@@ -4,7 +4,7 @@ import StudiosAPIContext from "../../../contexts/StudiosAPIContext";
 import {Table, Paper, TableContainer, TableHead, TableRow, TableCell, TableBody, Link} from "@mui/material";
 import { generatePath } from "react-router-dom";
 
-const StudiosTable = ({ perPage, params }) => {
+const StudiosTable = ({ perPage, page }) => {
     const { studios } = useContext(StudiosAPIContext);
 
     return (
@@ -22,7 +22,7 @@ const StudiosTable = ({ perPage, params }) => {
         <TableBody>
             {studios.map((studio, index) => (
                 <TableRow key={studio.id}>
-                    <TableCell>{ (params.page - 1) * perPage + index + 1 }</TableCell>
+                    <TableCell>{ (page - 1) * perPage + index + 1 }</TableCell>
                     <TableCell>
                         <Link href={generatePath("/studios/:id/profile", {id : studio.id })}
                               underline={"hover"}>
--- PF/fitness-club/src/pages/studios/index.js
+++ PF/fitness-club/src/pages/studios/index.js
@@ -2,20 +2,25 @@ import React, {useContext, useEffect, useState} from 'react';
 import {useNavigate} from "react-router-dom";
 import StudiosTable from "../../components/Studio/StudiosTable";
 import StudiosAPIContext from "../../contexts/StudiosAPIContext";
-import {Button} from "@mui/material";
+import {Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField} from "@mui/material";
 
 const Studios = () => {
-    const perPage = 20;
-    const [params, setParams] = useState({page: 1, search: ""})
+    const perPage = 5;
+    const [page, setPage] = useState(1);
+    const [open, setOpen] = useState(false);
+    const [filter, setFilter] = useState({});
+
+    const [offset, setOffset] = useState(0);
+
 
     const { setStudios } = useContext(StudiosAPIContext);
 
     let navigate = useNavigate();
 
-    useEffect(() => {
-        const {page, search} = params;
 
-        fetch(`http://localhost:8000/studios/search?limit=15`, {
+    useEffect(() => {
+        fetch(`http://localhost:8000/studios/search?offset=${offset}&limit=${perPage}&`
+            + new URLSearchParams(filter), {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
@@ -25,22 +30,97 @@ const Studios = () => {
             .then(response => response.json())
             .then(json => setStudios(json.results))
             .catch(err => console.log(err))
-    }, [params])
+    }, [filter, page, offset])
 
 
     const createStudios = () => {
         navigate('/studios/add');
     }
 
+    const handleOpen = () => {
+        setOpen(true);
+    }
+
+    const handleClose = () => {
+        setOpen(false);
+    }
+
+
+
+
     return (
         <>
             <h1> Studios </h1>
+            {/*{ isAdmin &&*/}
             <Button
                 variant="outlined"
                 onClick={createStudios}>
                 +
             </Button>
-            <StudiosTable perPage={perPage} params={params}></StudiosTable>
+            {/*}*/}
+            <Button variant={"outlined"} onClick={handleOpen}>FILTER</Button>
+            <Button variant={"outlined"} onClick={e => setFilter({})}>CLEAR FILTER</Button>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>Filter by...</DialogTitle>
+                <DialogContent>
+                    <TextField autoFocus id="name" label="Name" variant="standard"
+                               onChange={e => setFilter({
+                                ...filter,
+                                name__icontains: e.target.value})}/>
+
+                    <TextField autoFocus id="amenity-type" label="Amenity type" variant="standard"
+                               onChange={e => setFilter({
+                                   ...filter,
+                                   amenities__type__exact: e.target.value})}/>
+
+                    <TextField autoFocus id="amenity-quantity" type="number"
+                               label="Exact amenity quantity" variant="standard"
+                               onChange={e => setFilter({
+                                   ...filter,
+                                   amenities__quantity__exact: e.target.value})}/>
+
+                    <TextField autoFocus id="amenity-quantity-gte" type="number"
+                               label="Amenity quantity greater than or equal to.." variant="standard"
+                               onChange={e => setFilter({
+                                   ...filter,
+                                   amenities__quantity__gte: e.target.value})}/>
+
+                    <TextField autoFocus id="class-name" label="Class Name" variant="standard"
+                               onChange={e => setFilter({
+                                   ...filter,
+                                   classes__name__icontains: e.target.value})}/>
+
+                    <TextField autoFocus id="class-coach" label="Class Coach" variant="standard"
+                               onChange={e => setFilter({
+                                   ...filter,
+                                   classes__coach__icontains: e.target.value})}/>
+
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>APPLY</Button>
+                </DialogActions>
+
+            </Dialog>
+            <StudiosTable perPage={perPage} page={page}></StudiosTable>
+            <Button id={'prev-button'}
+                variant={"contained"}
+                onClick={() => {
+                    setPage(Math.max(1, page - 1));
+                    setOffset(offset => offset - perPage);
+                }}>
+                {`<`}
+            </Button>
+            <Button disabled={true}>{page}</Button>
+            <Button id={'next-button'}
+                variant={"contained"}
+                onClick={() => {
+                    setPage(page + 1);
+                    setOffset(offset => offset + perPage);
+                }}>
+                {`>`}
+            </Button>
+
+
 
         </>
     )
